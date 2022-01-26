a13b610552aaba69613e3d0f4b5f5c9656c4bcc9
nir <nirshahar@mail.tau.ac.il>
2022-01-27 00:25:22 +0200

    Change saving directory - For "riddle" answers - For "wars" survivors - For "wars" zombies

--- war/models.py
+++ war/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.core.validators import FileExtensionValidator, MinValueValidator
 from django.core.exceptions import ValidationError
 from codeguru.models import CgGroup
-from os.path import join
+from os.path import join, splitext
 from django.utils.crypto import get_random_string
 
 
@@ -27,15 +27,14 @@ class Challenge(models.Model):
 
 # TODO: change filename to defined pattern with group name and war
 
-def war_directory_path(instance, filename):
-    name = instance.group.name.replace(" ", "_") + filename
+def war_directory_path(instance, filename, file_type):
     if instance.group is None:
-        return join(instance.war.id, 'zombies', filename)
-    return join(instance.war.id, instance.group.id, filename)
+        return join("wars", "wars_zombies", instance.war.id, 'zombies', splitext(filename)[0] + "." + file_type)
+    return join("wars", "wars_submissions", instance.war.id, instance.group.id, splitext(filename)[0] + "_survivor." + file_type)
 
 
 def riddle_directory_path(instance, filename):
-    return join(str(instance.riddle.id), filename)
+    return join("riddles", str(instance.riddle.id), instance.group.id, "solution.zip")
 
 
 class War(Challenge):
@@ -62,8 +61,8 @@ class Survivor(models.Model):
     group = models.ForeignKey(
         CgGroup, null=True, editable=False, on_delete=models.CASCADE)
     war = models.ForeignKey(War, null=True, on_delete=models.CASCADE)
-    asm_file = models.FileField(upload_to=war_directory_path, validators=[asm_max])
-    bin_file = models.FileField(upload_to=war_directory_path, validators=[bin_max])
+    asm_file = models.FileField(upload_to=lambda instance, filename: war_directory_path(instance, filename, "asm"), validators=[asm_max])
+    bin_file = models.FileField(upload_to=lambda instance, filename: war_directory_path(instance, filename, "dat"), validators=[bin_max])
     result = models.PositiveIntegerField(default=0)
 
 class Riddle(Challenge):
