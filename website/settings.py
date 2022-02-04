efda3c3ff76bd1cde1070ee2670ef1dfc598b49f
Ido Sharon <michdodim@gmail.com>
2022-02-04 23:23:56 +0200

    debug false

--- website/settings.py
+++ website/settings.py
@@ -17,7 +17,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-IS_PRODUCTION = False
+IS_PRODUCTION = True
 
 LANGUAGE_CODE = 'en'
 # Quick-start development settings - unsuitable for production
